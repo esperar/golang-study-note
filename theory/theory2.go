ea05ca89ed107956c4409e643726f8bb58ddeede
aesperer <piyrw9754@gmail.com>
2023-04-04 20:19:36 +0900

    switch case

--- theory/theory2.go
+++ theory/theory2.go
@@ -26,6 +26,17 @@ func canIDrink(age int) bool {
 	return true
 }
 
+func canIDrink2(age int) bool {
+	switch koreanAge := age + 2; koreanAge {
+	case 10:
+		return false
+	case 18:
+		return true
+	}
+	return false
+}
+
 func IfElse() {
 	fmt.Println(canIDrink(18))
+	fmt.Println(canIDrink2(8))
 }
