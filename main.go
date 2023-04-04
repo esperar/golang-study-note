bb1bc6caeb3a636c63f355f24df1ab64a1429049
aesperer <piyrw9754@gmail.com>
2023-04-04 16:09:36 +0900

    Function Part One

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	fmt.Println("Hello World!")
 
-	something.SayHello() // 패키지와 함수 호출 임포트
-	theory.HelloName()   // 변수와 상수
+	something.SayHello()      // 패키지와 함수 호출 임포트
+	theory.HelloName()        // 변수와 상수
+	theory.FunctionsPartOne() // 함수 파트 1
 }
--- theory/theory.go
+++ theory/theory.go
@@ -1,6 +1,9 @@
 package theory
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func HelloName() {
 	const name = "hope"
@@ -10,4 +13,25 @@ func HelloName() {
 	var name1 string = "kim"
 	name1 = "hope2"
 	fmt.Println(name1)
+}
+
+func FunctionsPartOne() {
+	fmt.Println(multiply(2, 2))
+	fmt.Println(lenAndUpper("kim hope"))
+	totalLength, upperName := lenAndUpper("kim hope")
+	// totalLength, _ 이런식으로 작성하면 값 하나를 무시하고 앞의 값만 받을 수 있다.
+	fmt.Println(totalLength, upperName)
+	repeatMe("eee", "aaa", "bbb", "ccc")
+}
+
+func multiply(a, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
 }
